24be04845f7b411fdb3299bd62eb107192362dda
chenmc <ccurricane@gmail.com>
2021-01-05 18:51:52 +0800

    增加 httpGetter,PeerGetter的实现

--- http.go
+++ http.go
@@ -1,14 +1,18 @@
 package ccache
 
 import (
+	"io/ioutil"
+	"net/url"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
 )
 
-const defaultBasePath = "/_ccache/"
-
+const (
+	defaultBasePath = "/_ccache/"
+	defaultResplicas = 50
+)
 // HTTPPool implements PeerPicker for a pool of HTTP peers.
 type HTTPPool struct {
 	// this peer's base URL, e.g. "https://example.net:8000"
@@ -63,3 +67,39 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// w.Header().Set("Content-Type", "text/plain")
 	w.Write(view.ByteSlice()) // 复制一份再传输
 }
+
+// 创建具体的 HTTP 客户端类 httpGetter，实现 PeerGetter 接口
+type httpGetter struct {
+	baseURL string // 将要访问的远程节点的地址，例如 http://example.com/_geecache/。
+}
+
+// Get 通过group key 获取bytes
+func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+	// 组件约定的url，如 /<basepath>/<groupname>/<key>
+	u := fmt.Sprintf(
+		"%v%v/%v",
+		h.baseURL,
+		url.QueryEscape(group),
+		url.QueryEscape(key)
+	)
+
+	res, err := http.Get(u)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("server returned: %v", res.Status)
+	}
+
+	bytes, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("reading response body: %v", err)
+	}
+
+	return bytes, nil
+}
+
+// 类型断言， 判断*httpGetter 有没有实现PeerGetter接口，如果没有编译器会报错
+var _ PeerGetter = (*httpGetter)(nil) // 因为是指针类型实现，用nil类型转换为*httpGetter
